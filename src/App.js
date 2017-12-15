b9896254468ff8905085dbcad92d2d86a84b16a2
Mark Nielsen <marknielsen7@outlook.com>
2017-12-15 14:18:43 -0700

    router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import logo from './logo.svg';
+import router from './router.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {router}
       </div>
     );
   }
 }
 
-export default App;
+export default withRouter(App);
--- src/router.js
+++ src/router.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import CreateCard from './Components/View/CreateCard';
-import Favorites from './Components/View/Favorites';
-import Landing from './Components/View/Landing';
-import Home from './Components/View/Home';
-import MyDecks from './Components/View/MyDecks';
-import Search from './Components/View/Search';
-import Wrapper from './Components/View/Wrapper';
-import editDeck from './Components/Children/EditDeck';
-import createDeck from './Components/Children/CreateDeck';
-import StudyCarousel from './Components/Children/StudyCarousel';
+import Auth from './Views/Auth';
+import AddBook from './Views/AddBook';
+import BookDetails from './Views/BookDetails';
+import Bookshelf from './Views/Bookshelf';
+import Browsing from './Views/Browsing';
+import Cart from './Views/Cart';
+import EditBook from './Views/EditBook';
 
 
 export default (
@@ -17,14 +14,11 @@ export default (
         <Switch>
             <Route exact path='/' component={Auth}/>
             <Route path='/addbook' component={AddBook}/>
-            <Route path='/' component={MyDecks}/>
-            <Route path='/favorites' component={Favorites}/>
-            <Route path='/search' component={Search}/>
-            <Route path='/create-card/:deck_id' component={CreateCard}/>
-            <Route path='/viewer/:deck_id' component={Wrapper}/>    
-            <Route path='/edit-deck/:deck_id' component={editDeck}/>
-            <Route path='/create-deck/:deck_id' component={createDeck}/>
-            <Route path= '/study' component={StudyCarousel} />
+            <Route path='/bookdetails' component={BookDetails}/>
+            <Route path='/bookshelf' component={Bookshelf}/>
+            <Route path='/browsing' component={Browsing}/>
+            <Route path='/cart' component={Cart}/>
+            <Route path='editbook' component={EditBook}/>    
         </Switch>
     </div>
 )
